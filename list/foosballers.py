7526b2fbf361f3d6aeb12843e390aac416c30b80
willianchu <willianchu@hotmail.com>
2022-04-06 18:11:41 -0700

    work with lists finish

--- list/foosballers.py
+++ list/foosballers.py
@@ -39,8 +39,44 @@ print(medianStart)
 print(medianEnd)
 print(foosballers[medianStart:medianEnd])
 
-#
+# add an fake player in the middle of the list
+foosballers.insert(median,"Average Player")
+print(foosballers)
 
+outOfTheLeague = [ "Johnson", "Trump", "Ronald", "Michael", "Elvis"]
+foosballers.extend(outOfTheLeague)
+print(foosballers)
+
+# findPosition = foosballers.index("Average Player")
+foosballers.remove("Average Player")
+print(foosballers)
+
+# median player index
+median = len(foosballers)//2 #integer division
+print(foosballers[median])
+
+# add an fake player in the middle of the list
+foosballers.insert(median,"Average Player")
+print(foosballers)
+
+"""
+- Lacy is one spot ahead of Hubert
+
+- Omar is one spot behind Rebecca
+
+- Otto is 8th best in the league
+
+- Chauncey is 10 spots from the bottom of the league
+"""
+first = 0
+foosballers[first] = "Lacy"
+findPosition = foosballers.index("Rebecca")
+foosballers.insert(findPosition + 1, "Omar")
+position = 8
+realIndex = position - 1
+foosballers.insert(realIndex, "Otto")
+foosballers.insert(-10, "Chauncey")
+print(foosballers)
 
 
 
